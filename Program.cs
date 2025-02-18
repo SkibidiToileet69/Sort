8ecac41872e793d983b07f562186b4085f027e28
MEGA <MEGA@gmail.com>
2025-02-18 14:41:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Sort
+{
+    
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students =
+            {
+            new Student("Стефан", 15),
+            new Student("Иван", 17),
+            new Student("Йоан", 16),
+            new Student("Петър", 14)
+        };
+
+            BubbleSort(students);
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Name} - {student.Age} години");
+            }
+        }
+
+        static void BubbleSort(Student[] array)
+        {
+            int n = array.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j].Age > array[j + 1].Age)
+                    {
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Sort
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+       
+    }
+}
